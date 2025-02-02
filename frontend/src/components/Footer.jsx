aea55e75ce8f21f277948893ddcb7546f4fe37c4
vivek vemula <vivekvemula21@gmail.com>
2025-02-02 16:31:55 +0530

    home page and footer updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,17 +1,19 @@
+import { Link } from "react-router-dom";
+
+
 const Footer = () => {
 	return (
 		<footer className='py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800'>
 			<div className='flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row'>
-				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
-					Built by{" "}
-					<a
-						href='https://github.com/viVeK21111/netflix_clone'
-						target='_blank'
-						className='font-medium underline underline-offset-4'
-					>
-						you
-					</a>
-					. The source code is available on{" "}
+				<div className='flex items-center gap-4'>
+					<li className="list-none flex items-center gap-4">
+						<Link to='/' className='text-balance'> Home </Link>
+						<Link to='/tv' className='text-balance'> TV Shows </Link>
+						<Link to='/movies' className='text-balance'> Movies </Link>
+					</li>
+					</div>
+			<p>
+					 The source code is available on{" "}
 					<a
 						href='https://github.com/viVeK21111/netflix_clone'
 						target='_blank'
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className='hero-bg h-screen'></div>
+    <div className='hero-bg h-screen'>
+      <header className='max-w-6xl mx-auto flex items-center justify-center p-4'>
+        <div className='flex items-center'>
+          <img src='/klogo.png' alt='logo' className='w-24' />
+          <h1 className='text-red-700 font-bold text-7xl'>Flix</h1>
+        </div>
+      </header>
+      <div className='flex items-center justify-center mt-20 mx-3'>
+        <div className='w-full max-w-md bg-black/70 shadow-md p-8 rounded-lg'>
+          <h1 className='text-2xl text-white mb-4 font-bold text-center'>Welcome to Flix</h1>
+          <p className='text-white text-center'>Flix is a platform where you can watch your favorite movies and TV shows.</p>
+          <div className='flex justify-center mt-4'>
+            <Link to={'/signup'}>
+            <button className='py-2 px-4 bg-red-500 text-white rounded-md font-semibold hover:bg-red-700'>Get Started
+            </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
