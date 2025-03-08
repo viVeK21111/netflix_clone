d07ef30361d0b788e984e3633bbd0ae90f67273d
vivek vemula <vivekvemula21@gmail.com>
2025-03-08 15:06:52 +0530

    .env file removed

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
 dotenv.config();
+console.log("jwt secret"+process.env.JWT_SECRET)
 
 export const generateToken = (userId,res) => {
     const token = jwt.sign({id:userId},process.env.JWT_SECRET,{
@@ -10,7 +11,7 @@ export const generateToken = (userId,res) => {
     res.cookie("token",token,{
     maxAge: 15*24*60*60*1000, // in milliseconds
     httpOnly: true, // prevent xss attacks (cross site scripting)
-    secure: false, // send cookie only in https (true in deployment)
-    sameSite: 'none'
+    secure: true, // send cookie only in https (true in deployment)
+    sameSite: 'none',
     });
 }
